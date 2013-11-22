6d55bd201781c0abb274d6fc97af0c459c9845e9
light88 <vlight88@gmail.com>
2013-11-22 17:02:03 +0200

    base app

---
+++ DD/src/main/java/com/dev/moneykeeper/MoneyKeeperApp.java
@@ -0,0 +1,18 @@
+package com.dev.moneykeeper;
+
+import android.app.Application;
+
+/**
+ * DD
+ * Created by light on 11/2/13.
+ */
+public class MoneyKeeperApp extends Application {
+
+    private static final String TAG = "App";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
